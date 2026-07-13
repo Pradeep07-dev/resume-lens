d066b79f073147f680b441021d83a41b051b7572
Pradeep07-dev <pr875136@gmail.com>
2026-07-13 23:47:00 +0530

    added footer for home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "../components/home/Hero";
 import Features from "../components/home/Features";
-import Testimonials from "../components/home/Testimonials";
 import CallToAction from "../components/home/CallToAction";
 import Footer from "../components/home/Footer";
 
@@ -9,6 +8,9 @@ const Home = () => {
   return (
     <div className="w-full overflow-x-hidden">
       <Hero />
+      <Features />
+      <CallToAction />
+      <Footer />
     </div>
   );
 };
